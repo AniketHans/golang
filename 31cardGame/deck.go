116ca0c067ccefe235c8a6f947fe44d6edb01ca3
Aniket Hans <aniketdharya@gmail.com>
2025-02-10 00:03:14 +0530

    Modified notes.,d

--- 31cardGame/deck.go
+++ 31cardGame/deck.go
@@ -5,6 +5,22 @@ import "fmt"
 // creating a new type of []string called deck
 type deck []string
 
+func newDeck() deck{
+	cardSuits := []string{"Spades","Hearts","Diamonds","Clubs"}
+	cardValues := []string{"Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"}
+
+	cards := deck{}
+
+	for _, suit := range cardSuits{
+		for _, value := range cardValues{
+			cards = append(cards, fmt.Sprintf("%v of %v",value,suit))
+		}
+	}
+
+	return cards
+
+}
+
 func (d deck) printCards(){
 	for i, card := range d{
 		fmt.Println(i,card)
--- 31cardGame/main.go
+++ 31cardGame/main.go
@@ -1,16 +1,16 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Welcome to card game")
-	cards := deck{"Ace of Diamonds","King of Hearts"}
-	// cards2 := []string{"Ace of Diamonds","King of Hearts"}
+	// fmt.Println("Welcome to card game")
+	// cards := deck{"Ace of Diamonds","King of Hearts"}
+	// // cards2 := []string{"Ace of Diamonds","King of Hearts"}
 
-	cards.printCards()
-	// cards2.printCards() //This will throw error.
+	// cards.printCards()
+	// // cards2.printCards() //This will throw error.
 
+	// cards.addCard()
+	// cards.printCards()
 
-	cards.addCard()
+	cards := newDeck()
 	cards.printCards()
 }
